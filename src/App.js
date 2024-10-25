38728f71c6a1395c40bd869c951809da98720f5b
Xinran <xinrancao@hotmail.com>
2024-10-24 21:50:32 -0400

    ADD: add split window control

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import CodeEditor from "./components/editor/CodeEditor";
 import FileExplorer from "./components/editor/FileExplorer";
 import Preview from "./components/preview/Preview";
+import { Mosaic, MosaicWindow } from "react-mosaic-component";
 
 const initialFiles = [
   {
@@ -17,9 +18,27 @@ const initialFiles = [
   },
 ];
 
+const MY_MOSAIC_IDs = {
+  explorer: "File Explorer",
+  editor: "Code Editor",
+  preview: "Preview",
+};
+
 function App() {
   const [files, setFiles] = useState(initialFiles);
   const [currentFileIndex, setCurrentFileIndex] = useState(0);
+
+  const initialLayout = {
+    direction: "row",
+    first: MY_MOSAIC_IDs.explorer,
+    second: {
+      direction: "row",
+      first: MY_MOSAIC_IDs.editor,
+      second: MY_MOSAIC_IDs.preview,
+      splitPercentage: 50,
+    },
+    splitPercentage: 20,
+  };
 
   const handleFileSelect = (index) => {
     setCurrentFileIndex(index);
@@ -31,26 +50,51 @@ function App() {
     setFiles(newFiles);
   };
 
-  // console.log("files", files);
-
   return (
     <div className="App">
       <header className="header">
         <h1>CodeMirror + SWC WASM</h1>
       </header>
       <section className="editor">
-        <div>
-          <FileExplorer
-            files={files}
-            currentFileIndex={currentFileIndex}
-            onSelectFile={handleFileSelect}
-          />
-        </div>
-        <CodeEditor
-          file={files[currentFileIndex]}
-          onUpdateFile={handleFileUpdate}
+        <Mosaic
+          renderTile={(id, path) => {
+            switch (id) {
+              case MY_MOSAIC_IDs.explorer:
+                return (
+                  <MosaicWindow path={path} toolbarControls={[]} title="Files">
+                    <FileExplorer
+                      files={files}
+                      currentFileIndex={currentFileIndex}
+                      onSelectFile={handleFileSelect}
+                    />
+                  </MosaicWindow>
+                );
+              case MY_MOSAIC_IDs.editor:
+                return (
+                  <MosaicWindow path={path} toolbarControls={[]} title="Editor">
+                    <CodeEditor
+                      file={files[currentFileIndex]}
+                      onUpdateFile={handleFileUpdate}
+                    />
+                  </MosaicWindow>
+                );
+              case MY_MOSAIC_IDs.preview:
+                return (
+                  <MosaicWindow
+                    path={path}
+                    toolbarControls={[]}
+                    title="Preview"
+                  >
+                    <Preview fileContent={{ ...files[currentFileIndex] }} />
+                  </MosaicWindow>
+                );
+              default:
+                return null;
+            }
+          }}
+          initialValue={initialLayout}
+          className="mosaic-blueprint-theme"
         />
-        <Preview fileContent={{ ...files[currentFileIndex] }} />
       </section>
     </div>
   );
